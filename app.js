e7b196c407e14626a5526fcbda3411277f852f0e
MonicaBidart <monica.bidart@gmail.com>
2023-11-30 09:11:51 -0300

    Add Layouts

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const expressLayout = require("express-ejs-layouts");
 
 app.use(express.static("public"));
 
 app.set("view engine", "ejs"); // Seteo el motor de vistas configurándolo
 app.set("views", "./src/views"); // Indico donde están las vistas para que las busque ahí
+
+app.use(expressLayout);
+app.set("layout", "layouts/layout")
 
 app.use(require("./src/routes/tareasRouter"));
 
