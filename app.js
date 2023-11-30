8ea0f9bedf8627eb5fba672cac23c78b561f94af
MonicaBidart <monica.bidart@gmail.com>
2023-11-30 13:13:25 -0300

    Update landing Tareas

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.set("view engine", "ejs"); // Seteo el motor de vistas configurándolo
 app.set("views", "./src/views"); // Indico donde están las vistas para que las busque ahí
 
 app.use(expressLayout);
-app.set("layout", "layouts/layout")
+app.set("layout", "layouts/layout");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use(require("./src/routes/tareasRouter"));
 
--- src/controllers/tareasController.js
+++ src/controllers/tareasController.js
@@ -1,7 +1,30 @@
+// Nuestra fuente de datos
+let tareas = [
+  { id: 1, title: "Tarea 1", completed: false },
+  { id: 2, title: "Tarea 2", completed: false },
+  { id: 3, title: "Tarea 3", completed: false },
+  { id: 4, title: "Tarea 4", completed: false },
+  { id: 5, title: "Tarea 5", completed: false },
+  { id: 6, title: "Tarea 6", completed: false },
+];
+
 const index = (req, res) => {
-  res.render("index");
+  res.render("index", { tareas });
+};
+
+const store = (req, res) => {
+  const tarea = {
+    id: Date.now(),
+    title: req.body.title,
+    completed: false,
+  };
+
+  tareas.push(tarea);
+
+  res.redirect("/");
 };
 
 module.exports = {
   index,
+  store,
 };
